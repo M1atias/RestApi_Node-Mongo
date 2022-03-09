80cc6dfe2ff9691e560b8c5b3f054ef6948eb863
M1atias <maty.f.molina4a@gmail.com>
2022-03-09 19:47:43 -0300

    add: Se crearon los endpoints para el auth

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from 'morgan'
 import pkg from '../package.json'
 import productRoutes from './routes/products.routes'
+import authRoutes from './routes/auth.routes'
 
 const app = express()
 
@@ -22,6 +23,7 @@ app.get('/', (req,res)=>{
     })
 })
 
-app.use('/products',productRoutes)
+app.use('/api/products',productRoutes)
+app.use('/api/auth',authRoutes)
 
 export default app;
