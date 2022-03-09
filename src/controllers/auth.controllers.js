29c9542d96468cfbb591de557e4a8fccfa877a80
M1atias <maty.f.molina4a@gmail.com>
2022-03-09 20:29:38 -0300

    add: creo usuarios

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,7 +1,24 @@
 import User from "../models/User";
+import jwt from 'jsonwebtoken';
+import config from "../config";
 
 export const signUp = async(req, res) =>{
-    res.json('signUp')
+
+    const {username, email, password, roles} = req.body;
+
+    const newUser = new User({
+        username,
+        email,
+        password: await User.encryptPassword(password)
+    })
+    
+    const savedUser = await newUser.save();
+
+    const toker = jwt.sign({id: savedUser._id}, config.SECRET, {
+        expiresIn:86400 //24 horas
+    })
+
+    res.status(200).json({toker})
 }
 
 export const signIn = async(req,res) =>{
