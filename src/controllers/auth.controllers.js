8042e071befdc155d975d52999dc475689b537b7
M1atias <maty.f.molina4a@gmail.com>
2022-03-10 18:06:01 -0300

    add: Funciona el signin

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -23,7 +23,7 @@ export const signUp = async(req, res) =>{
 
     const savedUser = await newUser.save();
     console.log(savedUser)
-    
+
     const toker = jwt.sign({id: savedUser._id}, config.SECRET, {
         expiresIn:86400 //24 horas
     })
@@ -32,5 +32,17 @@ export const signUp = async(req, res) =>{
 }
 
 export const signIn = async(req,res) =>{
-    res.json('signIn')
+    const userFOund = await User.findOne({email: req.body.email}).populate('roles');
+
+    if (!userFOund) return res.status(400).json({message:'User not found'})
+
+    const matchPassword = await User.comparePassword(req.body.password, userFOund.password)
+
+    if (!matchPassword) return res.status(401).json({token:null, message:' Invalid pass'})
+
+    const token = jwt.sign({id: userFOund._id}, config.SECRET, {
+        expiresIn: 86400
+    })
+
+    res.json({token})
 }
